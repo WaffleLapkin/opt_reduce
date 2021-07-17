8611d00daccf1af7d69cb01d2fcb1fe749a1b923
Waffle <waffle.lapkin@gmail.com>
2021-07-17 14:20:37 +0300

    Remove std PR names from documentation and README

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,9 @@
 //! merge two options together.
 //!
 //! This method was previously proposed for addition to `std` two times but both
-//! PRs were closed.
-//! 1. Add `Option::merge` under `option_merge` feature gate [#84695][first PR]
-//! 2. Added `Option::reduce` [#87036][second PR]
+//! PRs were closed:
+//! 1. [#84695][first PR]
+//! 2. [#87036][second PR]
 //!
 //! [first PR]: https://github.com/rust-lang/rust/pull/84695
 //! [second PR]: https://github.com/rust-lang/rust/pull/87036
